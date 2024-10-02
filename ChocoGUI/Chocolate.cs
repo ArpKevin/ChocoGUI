ee38a1deb5f5e7a939f69990cd4e47d5198fd246
Kevin <arpaskevin11@gmail.com>
2024-10-02 08:36:13 +0200

    Add project files.

---
+++ ChocoGUI/Chocolate.cs
@@ -0,0 +1,27 @@
+﻿namespace ChocoGUI
+{
+    internal class Chocolate
+    {
+        public Chocolate(string productName, string chocolateType, int price, string productType, int cocoaContent, int netWeight)
+        {
+            ProductName = productName;
+            ChocolateType = chocolateType;
+            Price = price;
+            ProductType = productType;
+            CocoaContent = cocoaContent;
+            NetWeight = netWeight;
+        }
+
+        public string ProductName { get; set; }
+        public string ChocolateType { get; set; }
+        public int Price { get; set; }
+        public string ProductType { get; set; }
+        public int CocoaContent { get; set; }
+        public int NetWeight { get; set; }
+
+        public override string ToString()
+        {
+            return $"Csokoládé neve: {ProductName}, csokoládé típusa: {ChocolateType}, csokoládé ára: {Price}, termék típusa: {ProductType}, kakaótartalom: {CocoaContent}%, nettó tömeg: {NetWeight}g";
+        }
+    }
+}
---
+++ ChocoGUI/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChocoGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal List<Chocolate> chocolates { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            chocolates = new List<Chocolate>();
+
+            string chocoSrc = @"..\..\..\src\choco.txt";
+
+            using StreamReader srChoco = new StreamReader(chocoSrc);
+
+            _ = srChoco.ReadLine();
+
+            while (!srChoco.EndOfStream)
+            {
+                var x = srChoco.ReadLine().Split(";");
+                chocolates.Add(new Chocolate(x[0], x[1], int.Parse(x[2]), x[3], int.Parse(x[4]), int.Parse(x[5])));
+            }
+
+            lblHowManyTypesToChooseFrom.Text = $"{chocolates.GroupBy(c => c.CocoaContent).Count()} különböző termék típusunk van.";
+
+        }
+    }
+}
